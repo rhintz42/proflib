30a0599597226f2497db14301c736edb5da62cd0
Robert Hintz <roberth@surveymonkey.com>
2014-04-26 04:12:35 -0700

    Remove unnecessary files and incorporate parsefilelib into function_details

--- proflib/models/func_decorators.py
+++
@@ -1,2 +0,0 @@
-# TODO: This class contains all of the decorators of a function
-pass
--- proflib/models/function_details.py
+++ proflib/models/function_details.py
@@ -2,10 +2,10 @@ import sys
 import os
 import time
 import traceback
-from proflib.models.func_code import FuncCode
-from proflib.models.func_docstring import FuncDocstring
+from parsefilelib.models.file_obj import FileObj
+from parsefilelib.models.base_lines_obj import BaseLinesObj
 
-class FunctionDetails(object):
+class FunctionDetails(BaseLinesObj):
     """
     Encapsulates the details of the Function that don't have todo with a
         specific frame. These details include:
@@ -17,29 +17,74 @@ class FunctionDetails(object):
     """
 
     def __init__(self, py_frame, **kwargs):
-        self.function_name = py_frame.f_code.co_name
-        self.file_path = py_frame.f_code.co_filename
         self.line_number = py_frame.f_code.co_firstlineno
-        self.func_code = FuncCode(self)
-        self.func_docstring = FuncDocstring(self)
+        #self.file_obj = self.fetch_file_obj(py_frame.f_code.co_filename)
+
+        line_number = py_frame.f_code.co_firstlineno
+        function_name = py_frame.f_code.co_name
+        file_path = py_frame.f_code.co_filename
+
+        super(FunctionDetails, self).__init__(def_name=function_name,
+                                                file_path=file_path)
+        if self.has_file:
+            # TODO: PASS IN LINE NUMBER TO THIS FUNCTION SO CAN BE SURE HAVE
+            #   CORRECT FUNCTION, OTHERWISE CAN"T TELL BETWEEN DIFFERENT CLASSES
+            self.function_obj = self.fetch_function_obj(py_frame.f_code.co_name)
+
+        if not self.has_function:
+            self.function_name = py_frame.f_code.co_name
+            self.file_path = py_frame.f_code.co_filename
     
     """ GETTERS """
+    @property
+    def has_file(self):
+        if self.file_obj:
+            return True
+        return False
+
+    @property
+    def has_function(self):
+        if self.function_obj:
+            return True
+        return False
+
     @property
     def code(self):
         """
         Return the code for this function
+
+        Alias for lines
         """
-        return self.func_code.code
+        return self.lines
+
+    #@property
+    #def lines(self):
+    #    """
+    #    Return the code for this function
+    #    """
+    #    if self.has_function:
+    #        return self.function_obj.lines
+    #    return []
+
+    #@property
+    #def decorators(self):
+    #    """
+    #    Return the code for this function
+    #    """
+    #    if self.has_function:
+    #        return self.function_obj.decorators
+    #    return []
 
     # TODO: ADD FORMATTED DOCSTRING METHOD
-    @property
-    def docstring(self):
-        """
-        Return the docstring for this function
-        """
-        return self.func_docstring.docstring
+    #@property
+    #def docstring(self):
+    #    """
+    #    Return the docstring for this function
+    #    """
+    #    if self.has_function:
+    #        return self.function_obj.docstring
+    #    return self._docstring
 
-    # TODO: ADD FILEPATH FUNCTION
     @property
     def file_name(self):
         """
@@ -53,7 +98,14 @@ class FunctionDetails(object):
         """
         Returns the file_path that this function resides in
         """
-        return self._file_path
+        return self.file_obj.path
+
+    @property
+    def file_obj(self):
+        """
+        Returns the file object
+        """
+        return self.parent_file
 
     # TODO: CHANGE function_name TO name
     #   * it's just redundant if I keep function_name
@@ -62,21 +114,18 @@ class FunctionDetails(object):
         """
         Return the name of the function
         """
+        if self.has_function:
+            return self.function_obj.name
         return self._function_name
 
     @property
-    def func_code(self):
+    def function_obj(self):
         """
-        Return the func_code object associated with this function
+        Return the function object gotten from the file_obj
         """
-        return self._func_code
-
-    @property
-    def func_docstring(self):
-        """
-        Return the func_docstring object associated with this function
-        """
-        return self._func_docstring
+        if hasattr(self, '_function_obj'):
+            return self._function_obj
+        return None
 
     @property
     def line_number(self):
@@ -86,12 +135,15 @@ class FunctionDetails(object):
         return self._line_number
 
     """ SETTERS """
-    @docstring.setter
-    def docstring(self, value):
-        """
-        Sets the value for the docstring object for this function
-        """
-        self._docstring = value
+    #@docstring.setter
+    #def docstring(self, value):
+    #    """
+    #    Sets the value for the docstring object for this function
+    #    """
+    #    if self.has_function:
+    #        self._function_obj.append_docstring(value)
+    #    else:
+    #        self._docstring = value
 
     @file_path.setter
     def file_path(self, value):
@@ -100,26 +152,30 @@ class FunctionDetails(object):
         """
         self._file_path = value
 
+    #@file_obj.setter
+    #def file_obj(self, value):
+    #    """
+    #    Sets the value for the file_obj for this function
+    #    """
+    #    self._file_obj = value
+
+    @function_obj.setter
+    def function_obj(self, value):
+        """
+        Sets the value for the function_obj for this function
+        """
+        self._function_obj = value
+
+    
     @function_name.setter
     def function_name(self, value):
         """
         Sets the value for the function_name for this function
         """
-        self._function_name = value
-
-    @func_code.setter
-    def func_code(self, value):
-        """
-        Sets the value for the func_code object
-        """
-        self._func_code = value
-
-    @func_docstring.setter
-    def func_docstring(self, value):
-        """
-        Sets the value for the func_docstring object
-        """
-        self._func_docstring = value
+        if self.has_function:
+            self._function_obj.name = value
+        else:
+            self._function_name = value
 
     @line_number.setter
     def line_number(self, value):
@@ -127,3 +183,26 @@ class FunctionDetails(object):
         Sets the value for the line number that this function is at
         """
         self._line_number = value
+
+
+    """ FETCH FUNCTIONS """
+    #def fetch_file_obj(self, path):
+    #    return FileObj(path)
+
+
+    def fetch_function_obj(self, function_name):
+        return self.rec_fetch_function_obj(self.file_obj, function_name)
+        
+
+    def rec_fetch_function_obj(self, obj, function_name):
+        for f in obj.functions:
+            if f.name == function_name:
+                return f
+
+        for f in obj.functions:
+            self.rec_fetch_function_obj(f, function_name)
+
+        for c in obj.functions:
+            self.rec_fetch_function_obj(c, function_name)
+
+        return None
