c8e4352eb43905ed5fb1d1c2996485dc8ace36db
Robert Hintz <roberthintz@gmail.com>
2014-09-02 14:11:49 -0700

    Add test for classmethods

--- tests/integration/lib/test_decorator.py
+++ tests/integration/lib/test_decorator.py
@@ -4,8 +4,27 @@ except:
     import unittest
 import sys
 
+from proflib.lib.decorators import prof
+
+
+class Foo(object):
+    @classmethod
+    @prof()
+    def bar(cls, a, b):
+        return {
+            'a': a,
+            'b': b,
+        }
+
 
 class TestProf(unittest.TestCase):
 
     def test_trace(self):
         pass
+
+    def test_class_method(self):
+        result = Foo.bar(10, 20)
+        assert result == {
+            'a': 10,
+            'b': 20,
+        }
